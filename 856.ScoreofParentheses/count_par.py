180b0a7b1318ce88841b804e72846789ff1de007
marathohoho <codmarat@gmail.com>
2020-04-04 22:13:50 +0500

    Added:_856.ScoreofParentheses

---
+++ 856.ScoreofParentheses/count_par.py
@@ -0,0 +1,43 @@
+'''
+Given a balanced parentheses string S, compute the score of the string based on the following rule:
+
+() has score 1
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+
+
+Example 1:
+
+Input: "()"
+Output: 1
+Example 2:
+
+Input: "(())"
+Output: 2
+Example 3:
+
+Input: "()()"
+Output: 2
+Example 4:
+
+Input: "(()(()))"
+Output: 6
+
+
+Note:
+
+S is a balanced parentheses string, containing only ( and ).
+2 <= S.length <= 50
+'''
+
+def count(S) :
+	stack = [0]
+
+	for c in S:
+		if c == "(" :
+			stack.append(0)
+		else :
+			value = stack.pop()
+			stack[-1] += max(2 * value, 1)
+
+	return stack.pop()
